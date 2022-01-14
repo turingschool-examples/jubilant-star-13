0820f27fd22bebfd6f50cd7648276c44b8a12f48
ParkerLockhart <parkerlockhart42@gmail.comm>
2022-01-14 11:19:41 -0600

    Model tests for instance methods

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,26 @@ RSpec.describe Hospital do
   describe 'relationships' do
     it { should have_many(:doctors) }
   end
+
+  describe 'instance methods' do
+    let!(:hospital) {Hospital.create!(name: "Grey Sloan Memorial Hospital")}
+    let!(:bailey) {hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")}
+    let!(:perez) {hospital.doctors.create!(name: "Sergio Perez", specialty: "Neurology", university: "UTHSC")}
+    let!(:patient_1) {perez.patients.create!(name: "Patient 1")}
+    let!(:patient_2) {perez.patients.create!(name: "Patient 2")}
+    let!(:patient_3) {bailey.patients.create!(name: "Patient 3")}
+    let!(:patient_4) {bailey.patients.create!(name: "Patient 4")}
+
+    describe '#doctor_count' do
+      it 'returns number of doctors for university' do
+        expect(hospital.doctor_count).to eq(2)
+      end
+    end
+
+    describe '#doctor_university_list' do
+      it 'returns list of universities for hospital doctors' do
+        expect(hospital.doctor_university_list).to eq(["Stanford University", "UTHSC"])
+      end
+    end
+  end 
 end
