a90f8a6c4014ee118a145c9080432c14ef6c8bfe
Tim Roeder <timothyjroeder@gmail.com>
2022-01-14 11:35:26 -0700

    Test: add ::age_ordered test

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe Patient, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :age }
   end
+
+  let!(:pat_1) { Patient.create!(name: 'pat_1', age: 1) }
+  let!(:pat_2) { Patient.create!(name: 'pat_2', age: 2) }
+  let!(:pat_3) { Patient.create!(name: 'pat_3', age: 3) }
+  let!(:pat_4) { Patient.create!(name: 'pat_4', age: 4) }
+
+  describe 'class methods' do
+    describe '::age_ordered' do
+      it 'returns a list of all patients ordered by age oldest to youngest' do
+        expect(Patient.all).to eq([pat_1, pat_2, pat_3, pat_4])
+        expect(Patient.age_ordered).to eq([pat_4, pat_3, pat_2, pat_1])
+      end
+    end
+  end
 end
