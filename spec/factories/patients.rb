ee4adb060d14884c8e17ff340ee1e73c17506efe
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-14 11:39:08 -0700

    Update patient factory to sequnce age. Write extension patient index feature specs.

--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :patient do
     sequence(:name) { |n| "Default Patient Name #{n}" }
-    age { 17 }
+    sequence(:age) { |n| 17 + n }
 
     factory :patient_with_doctor do
 
