084c62aa24dbb0438ae958bf8680e697e7fa532e
Hannah Warren <hannahkwarren@gmail.com>
2022-01-14 11:39:21 -0500

    Adds many to many relationship and models for doctors <=> patients

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -12,12 +12,12 @@ RSpec.describe "Doctor show page", type: :feature do
     # And I see the name of the hospital where this doctor works
     # And I see the names of all of the patients this doctor has
 
-    @hospital1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+    hospital1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
 
-    @doctor1 = Doctor.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University", hospital_id: @hospital1.id)
-    @doctor2 = Doctor.create!(name: "Alex Karev", specialty: "Pediatrics", university: "Washington State University", hospital_id: @hospital1.id)
+    doctor1 = Doctor.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University", hospital_id: hospital1.id)
+    doctor2 = Doctor.create!(name: "Alex Karev", specialty: "Pediatrics", university: "Washington State University", hospital_id: hospital1.id)
 
-    visit doctor_path(@doctor1)
+    visit doctor_path(doctor1)
 
     expect(page).to have_content("Name: Meredith Grey")
     expect(page).to have_content("Specialty: General Surgery")
@@ -29,16 +29,34 @@ RSpec.describe "Doctor show page", type: :feature do
   end
 
   it "has the name of the hospital where this doctor works" do 
-    @hospital1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+    hospital1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
 
-    @doctor1 = Doctor.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University", hospital_id: @hospital1.id)
-    @doctor2 = Doctor.create!(name: "Alex Karev", specialty: "Pediatrics", university: "Washington State University", hospital_id: @hospital1.id)
+    doctor1 = Doctor.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University", hospital_id: hospital1.id)
+    doctor2 = Doctor.create!(name: "Alex Karev", specialty: "Pediatrics", university: "Washington State University", hospital_id: hospital1.id)
 
-    visit doctor_path(@doctor1)
+    visit doctor_path(doctor1)
 
     expect(page).to have_content("Hospital: Grey Sloan Memorial Hospital")
   end
 
-  it "has the names of all the patients for this doctor"
+  it "has the names of all the patients for this doctor" do
+    hospital1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+
+    doctor1 = Doctor.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University", hospital_id: hospital1.id)
+    doctor2 = Doctor.create!(name: "Christina Yang", specialty: "Pediatrics", university: "Yale Medical School", hospital_id: hospital1.id)
+
+    patient1 = Patient.create!(name: "Derek Shepherd", age: 38)
+    doctor2.patients << patient1
+
+    patient2 = Patient.create!(name: "Henry Collins", age: 40)
+    doctor2.patients << patient2
+
+    visit doctor_path(doctor2)
+
+    within ".doctor-#{doctor2.id}-patients" do
+      expect(page).to have_content("Derek Shepherd, 38")
+      expect(page).to have_content("Henry Collins, 40")
+    end
+  end
 
 end
