c8ecc6a5d60cb067ce140719202da0cd65839dd6
Eric Chon <echon92@gmail.com>
2022-01-14 09:23:38 -0700

    relationships pass

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Doctor do
   describe 'relationships' do
     it { should belong_to(:hospital) }
     it { should have_many(:doctor_patients) }
-    it {should have_many(:patients).through :doctor_patients}
+    it { should have_many(:patients).through :doctor_patients}
   end
 end
