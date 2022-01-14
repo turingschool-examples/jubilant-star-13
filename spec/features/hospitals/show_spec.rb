a6ddaa9a273d0bbd8fb8ce5024c30d5dce7d1229
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-14 09:51:11 -0700

    Create test for hospital show page

---
+++ spec/features/hospitals/show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'hospital show page' do
+
+  it 'shows the hospitals name' do
+  end
+
+  it 'shows the number of doctors working at the hospital' do
+  end
+
+  it 'lits the unique universities that its doctors have attended' do
+  end
+end 
