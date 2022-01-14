bf6b9086476d425c3bd34408aab4311b0b33025f
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-14 12:43:53 -0500

    halfway through

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -4,4 +4,9 @@ class DoctorsController < ApplicationController
     @doctor = Doctor.find(params[:id])
     @hospital = @doctor.hospital
   end
+
+  def update
+    @doctor = Doctor.find(params[:id])
+    
+  end
 end
