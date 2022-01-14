af6a86b0742f233d7fd7ace13ecee9388bd91129
Russell Rockwood <russell.rockwood@gmail.com>
2022-01-14 12:11:07 -0500

    add hospital universities method

--- spec/features/hospitals/show_spec.rb
+++ spec/features/hospitals/show_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-describe 'doctor show page' do
-  before(:each) do
+describe 'hospital show page' do
+  it "shows the number of doctors at the hospital and universities" do
     @hospital_1 = Hospital.create!(name: 'hospital_1')
     @doctor_1 = Doctor.create!(name: 'doctor_1', specialty: 'specialty_1', university: 'university_1', hospital_id: @hospital_1.id)
     @doctor_2 = Doctor.create!(name: 'doctor_2', specialty: 'specialty_2', university: 'university_2', hospital_id: @hospital_1.id)
+    @doctor_3 = Doctor.create!(name: 'doctor_3', specialty: 'specialty_3', university: 'university_2', hospital_id: @hospital_1.id)
 
     @patient_1 = Patient.create!(name: 'patient_1', age: 20)
     @patient_2 = Patient.create!(name: 'patient_2', age: 21)
@@ -13,18 +14,11 @@ describe 'doctor show page' do
     DoctorPatient.create!(doctor_id: @doctor_1.id, patient_id: @patient_1.id)
     DoctorPatient.create!(doctor_id: @doctor_1.id, patient_id: @patient_2.id)
     DoctorPatient.create!(doctor_id: @doctor_2.id, patient_id: @patient_3.id)
-  end
 
-  it "lists doctor info and patient names" do
-    visit "/doctors/#{@doctor_1.id}"
-    
-    expect(page).to have_content("#{@doctor_1.name}")
-    expect(page).to have_content("hospital_1")
-    expect(page).to have_content("#{@doctor_1.university}")
-    expect(page).to have_content("#{@doctor_1.specialty}")
-    expect(page).to have_content("#{@patient_1.name}")
-    expect(page).to have_content("#{@patient_2.name}")
-    expect(page).to_not have_content("#{@patient_3.name}")
+    visit "/hospitals/#{@hospital_1.id}"
+    expect(page).to have_content(@hospital_1.name)
+    expect(page).to have_content("Doctors: 3")
+    expect(page).to have_content("university_1")
+    expect(page).to have_content("university_2")
   end
-
 end
