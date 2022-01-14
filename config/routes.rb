f5a5c31399cc8af5938e78725cdee7875d0443cb
Robin Dittrich <robinodittrich@gmail.com>
2022-01-14 09:35:28 -0800

    Create route for patient index page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "/doctors/:id", to: "doctors#show"
   get "/hospitals/:id", to: "hospitals#show"
   delete "/doctors", to: "doctor_patients#destroy"
+  get "/patients", to: "patients#index"
 end
