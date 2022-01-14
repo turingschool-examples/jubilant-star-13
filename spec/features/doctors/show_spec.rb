df4eca791a51c86067325f8a01e1171e20e62f67
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-14 09:21:18 -0700

    Create tests for doctor show page

---
+++ spec/features/doctors/show_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'doctor show page' do
+  it 'displays the doctors information' do
+  end
+
+  it 'displays the name of the hospital the doctor works at' do
+  end
+
+  it 'displays the names of all the patients the doctor has' do
+  end
+end 
