b76961a1e28fbc504e382f53adc6a1a5c2d88dd9
Mallory Vining <mallorygallery12@gmail.com>
2022-01-14 10:13:18 -0700

    Story 2 Pt 2

--- spec/features/hospitals/show_spec.rb
+++ spec/features/hospitals/show_spec.rb
@@ -3,10 +3,29 @@ require 'rails_helper'
 RSpec.describe "Hospital Show Page" do
   it "shows hospital info" do
     sloans = Hospital.create!({name: "Grey Sloan Memorial Hospital"})
+    other = Hospital.create!({name: "The Other Hospital"})
 
     visit "/hospitals/#{sloans.id}"
 
     expect(page).to have_content(sloans.name)
+    expect(page).to_not have_content(other.name)
+  end
+
+  it "shows the number of doctors that work at this hospital" do
+    sloans = Hospital.create!({name: "Grey Sloan Memorial Hospital"})
+    other = Hospital.create!({name: "The Other Hospital"})
+
+    miranda = sloans.doctors.create!({name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University"})
+    derrick = sloans.doctors.create!({name: "Derrick Shepard", specialty: "Brain Surgery", university: "Harvard University"})
+    christina = sloans.doctors.create!({name: "Christina Yang", specialty: "Heart Surgery", university: "Columbia University"})
+    george = other.doctors.create!({name: "George O'Malley", specialty: "Foot Surgery", university: "Brown University"})
+
+    visit "/hospitals/#{sloans.id}"
+
+    expect(page).to have_content("Number of Doctor's: 3")
+  end
+
+  it "shows a unique list of universities that this hospital's doctors attended" do
   end
 end
 # User Story 2, Hospital Show Page
