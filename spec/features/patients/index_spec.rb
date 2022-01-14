1935afe75b9d1cd35f1a4c0e163968b47d4cf17f
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-14 11:22:09 -0700

    Create test for patients index page

---
+++ spec/features/patients/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'patient index page' do
+  let(:denny) {Patient.create!(name: "Denny Duquette", age: 39)}
+  let(:bob) {Patient.create!(name: "Bob Duquette", age: 44)}
+  let(:larry) {Patient.create!(name: "Larry Duquette", age: 27)}
+
+  it 'lists the names of all patients in order of age, oldest to youngest' do
+    visit "/patients"
+    
+    expect(page).to have_content("Patients")
+    expect(bob.name).to appear_before(denny.name)
+    expect(denny.name).to appear_before(larry.name)
+  end
+end
