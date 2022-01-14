64a458f77bb6212ddd82e05bc9c1a7dec4dd2f7c
Sierra Tucker <sierra.tucker121@gmail.com>
2022-01-14 11:07:57 -0700

    Test: add employer class method

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -6,4 +6,18 @@ RSpec.describe Doctor do
     it { should have_many(:doctor_patients) }
     it { should have_many(:patients).through(:doctor_patients) }
   end
+
+  describe 'instance methods' do
+    before(:each) do
+      @hospital_1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+      @hospital_2 = Hospital.create!(name: "St. Mary's")
+      @doctor_1 = @hospital_1.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+    end
+
+    describe '#employer' do
+      it 'gives name of hospital where doctor works' do
+        expect(@doctor_1.employer).to eq(@hospital_1.name)
+      end
+    end
+  end
 end
