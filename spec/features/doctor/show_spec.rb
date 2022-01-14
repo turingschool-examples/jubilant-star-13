87d120000cb84b443c4dca6d70fac6bbc27719fe
Katy Harrod <m.catherine.harrod@gmail.com>
2022-01-14 11:52:53 -0700

    Add sad path for doctor show page spec

--- spec/features/doctor/show_spec.rb
+++ spec/features/doctor/show_spec.rb
@@ -3,16 +3,22 @@ require 'rails_helper'
 describe 'doctor show page' do
   before do
     @hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
-    
+
     @bailey = @hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+    @not_bailey = @hospital.doctors.create!(name: "not Bailey", specialty: "nun", university: "oooooooooooo")
 
     @patient1 = Patient.create!(name: "Grace", age: 14)
     @patient2 = Patient.create!(name: "Ian", age: 47)
 
-    @not_baileys_patient1 = Patient.create!
+    @not_baileys_patient1 = Patient.create!(name: "null", age: 0)
+    @not_baileys_patient2 = Patient.create!(name: "null", age: 5)
 
     @bailey_patient1 = PatientDoctor.create!(doctor_id: @bailey.id, patient_id: @patient1.id)
     @bailey_patient2 = PatientDoctor.create!(doctor_id: @bailey.id, patient_id: @patient2.id)
+
+    @not_bailey_patient1 = PatientDoctor.create!(doctor_id: @not_bailey.id, patient_id: @not_baileys_patient1.id)
+    @not_bailey_patient2 = PatientDoctor.create!(doctor_id: @not_bailey.id, patient_id: @not_baileys_patient2.id)
+
     visit doctor_path(@bailey)
   end
   it 'displays name, specialty, and university' do
@@ -27,6 +33,11 @@ describe 'doctor show page' do
     expect(page).to have_content(@patient2.name)
   end
 
+  it 'does not show patients who are cared for by different doctors' do
+    expect(page).not_to have_content(@not_baileys_patient1.name)
+    expect(page).not_to have_content(@not_baileys_patient2.name)
+  end
+
   it 'has button to remove patient' do
     within "#doctors_patients" do
       within "#patient-#{@patient1.id}" do
@@ -34,7 +45,4 @@ describe 'doctor show page' do
       end
     end
   end
-
-  # add more patients associated w different doctor
-  # for more robust testing after complete other stories
 end
