a6b1662985081dca3fb3ada66ebf24fecac03daf
Kevin Gloss <kgloss89@gmail.com>
2022-01-14 10:10:52 -0700

    Test doctor count and universities feats

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,38 @@ RSpec.describe Hospital do
   describe 'relationships' do
     it { should have_many(:doctors) }
   end
+
+  before :each do
+    @h1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+    @h2 = Hospital.create!(name: "Black Swan Memorial Hospital")
+
+    @d1 = @h1.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+    @d2 = @h1.doctors.create!(name: "Gray Anatomy", specialty: "Plastic Surgery", university: "Doctor University")
+    @d3 = @h1.doctors.create!(name: "John Smith", specialty: "Pediatrist", university: "Harvard University")
+    @d4 = @h1.doctors.create!(name: "Jane Smith", specialty: "Pediatrist", university: "Harvard University")
+    @d5 = @h2.doctors.create!(name: "Harold Riviera", specialty: "Cardiologist", university: "Columbia University")
+
+    @p1 = Patient.create!(name: "Marley Johanson", age: 37)
+    @p2 = Patient.create!(name: "Jones Johanson", age: 40)
+    @p3 = Patient.create!(name: "Tyler Smith", age: 30)
+    @p4 = Patient.create!(name: "Dan McNeil", age: 33)
+    @p5 = Patient.create!(name: "Wade Wilson", age: 45)
+
+    @dp1 = DoctorPatient.create!(doctor: @d1, patient: @p1)
+    @dp2 = DoctorPatient.create!(doctor: @d1, patient: @p2)
+    @dp3 = DoctorPatient.create!(doctor: @d1, patient: @p3)
+    @dp4 = DoctorPatient.create!(doctor: @d1, patient: @p4)
+    @dp5 = DoctorPatient.create!(doctor: @d2, patient: @p1)
+    @dp6 = DoctorPatient.create!(doctor: @d2, patient: @p5)
+  end
+
+  describe 'instance methods' do
+    it '#doctor_count' do
+      expect(@h1.doctor_count).to eq(4)
+    end
+
+    it '#universities' do
+      expect(@h1.universities).to eq(["Stanford University", "Doctor University, Harvard University"].sort)
+    end
+  end
 end
