b7bccb9576b50b088888afde0c701fb83db59265
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-14 11:39:07 -0700

    Feat: patient index displaying patients by age, oldest to youngest

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/patients', to: 'patients#index'
+
   get '/doctors/:id', to: 'doctors#show'
 
   delete '/doctors/:doctor_id/patients/:patient_id', to: 'doctor_patients#delete'
