305a623a8b29ccea0eb1e7f5c2957d3fe3b84cde
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-14 11:34:22 -0700

    Create test for patient::order_by_age

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Patient, type: :model do
     it { should have_many(:doctor_patients) }
     it { should have_many(:doctors).through(:doctor_patients) }
   end
+
+  describe '::order_by_age' do
+    let!(:denny) {Patient.create!(name: "Denny Duquette", age: 39)}
+    let!(:bob) {Patient.create!(name: "Bob Duquette", age: 44)}
+    let!(:larry) {Patient.create!(name: "Larry Duquette", age: 27)}
+    
+    it 'orders all patients by age, oldest to youngest' do
+      expect(Patient.order_by_age).to eq([bob, denny, larry])
+    end
+  end
 end
