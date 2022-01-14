3fe25cf31af7f7538d6f6b5afbb392dd26f7f755
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-14 09:42:50 -0700

    Fix and Re-migrate patients, doctor_patients to include correct data type

--- db/migrate/20220114163948_create_patient.rb
+++ db/migrate/20220114163948_create_patient.rb
@@ -1,7 +1,7 @@
-class CreatePatients < ActiveRecord::Migration[5.2]
+class CreatePatient < ActiveRecord::Migration[5.2]
   def change
     create_table :patients do |t|
-      t.integer :name
+      t.string :name
       t.integer :age
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_160945) do
+ActiveRecord::Schema.define(version: 2022_01_14_164046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2022_01_14_160945) do
   end
 
   create_table "patients", force: :cascade do |t|
-    t.integer "name"
+    t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
