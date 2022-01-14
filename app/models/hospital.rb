c0ca9fe8b161ba3e554398a49ecf6b6326718ac1
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-01-14 10:33:12 -0700

    Feat: hospital show page

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,10 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def doctor_count
+    doctors.count
+  end 
+  def assoc_universities
+    doctors.select("doctors.university").distinct
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/hospitals/:hospital_id/doctors/:doctor_id', to: 'hospital_doctors#show'
-  
 
+  get '/hospitals/:hospital_id', to: 'hospitals#show'
 end
