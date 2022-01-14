06ed5fa059b36bace26e87c6046a9ba2b1e6378f
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-01-14 10:59:32 -0700

    Test: patient index page

---
+++ spec/features/patients/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Patient Index Page' do
+  it 'displays all paitients in order from oldest to youngest' do
+    geoff = Patient.create!(name: "Geoff Banks", age: 34)
+    wilson = Patient.create!(name: "Wilson General", age: 44)
+    sharina = Patient.create!(name: "Sharina Triss", age: 22)
+
+    expect(page).to have_content("Patient Index")
+    expect("Wilson General").to appear_before("Geoff Banks")
+    expect("Geoff Banks").to appear_before("Sharina Triss")
+  end
+end 
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Patient, type: :model do
     it { should have_many(:doctor_patients) }
     it { should have_many(:doctors).through(:doctor_patients) }
   end
+
+  describe 'class methods' do
+    it '#older_first' do
+      geoff = Patient.create!(name: "Geoff Banks", age: 34)
+      wilson = Patient.create!(name: "Wilson General", age: 44)
+      sharina = Patient.create!(name: "Sharina Triss", age: 22)
+
+      expect(Patient.older_first).to eq([wilson, geoff, sharina])
+    end
+  end
 end
