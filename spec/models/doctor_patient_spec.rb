609d3d2f2408e4e3b4086a0719de6c4ec8e251ef
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-14 09:15:52 -0700

    Create relationship tests for models

--- spec/models/doctor_patient_spec.rb
+++ spec/models/doctor_patient_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe DoctorPatient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:doctor) }
+    it { should belong_to(:patient) }
+  end
 end
