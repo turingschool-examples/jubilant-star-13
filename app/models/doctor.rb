afd2ec46da7832772bf9228d7f730a2b9e7038ae
Karan Mehta <karanm645@gmail.com>
2022-01-14 11:46:44 -0600

    Create .unique_universities method

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,8 @@ class Doctor < ApplicationRecord
   belongs_to :hospital
   has_many :doctor_patients 
   has_many :patients, through: :doctor_patients
+
+  def self.unique_universities
+    select(:university).pluck(:university)
+  end
 end
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -6,5 +6,14 @@ RSpec.describe Doctor do
     it { should have_many :doctor_patients }
     it { should have_many(:patients).through(:doctor_patients)}
 
+    let!(:three_sisters) { Hospital.create!(name: "Three Sisters Hospital") }
+    let!(:samantha) { three_sisters.doctors.create!(name: "Samanta Mumba", specialty: "Cardiologist", university: "USC") }
+    let!(:lisa) { three_sisters.doctors.create!(name: "Lisa Left Eye Lopez", specialty: "Neurosurgeon", university: "UT") }
+    let!(:lauryn) { three_sisters.doctors.create!(name: "Lauryn Hill", specialty: "General", university: "Harvard") }
+    
+    describe 'class methods'
+    it '.unique_universities' do 
+      expect(three_sisters.doctors.unique_universities).to eq([samantha.university, lisa.university, lauryn.university])
+    end 
   end
 end
