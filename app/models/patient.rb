33aef63622df258f27e7898291384f1f65f7fe7a
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-01-14 11:09:05 -0700

    Feat: patient index page

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,8 @@
 class Patient < ApplicationRecord
   has_many :doctor_patients
   has_many :doctors, through: :doctor_patients
-end 
+
+  def self.older_first
+    Patient.order(age: :desc)
+  end
+end
--- spec/features/patients/index_spec.rb
+++ spec/features/patients/index_spec.rb
@@ -5,9 +5,10 @@ RSpec.describe 'Patient Index Page' do
     geoff = Patient.create!(name: "Geoff Banks", age: 34)
     wilson = Patient.create!(name: "Wilson General", age: 44)
     sharina = Patient.create!(name: "Sharina Triss", age: 22)
-
+    visit '/patients'
+    
     expect(page).to have_content("Patient Index")
     expect("Wilson General").to appear_before("Geoff Banks")
     expect("Geoff Banks").to appear_before("Sharina Triss")
   end
-end 
+end
