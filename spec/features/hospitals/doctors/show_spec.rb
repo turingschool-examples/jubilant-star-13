fa293e462fcc2b6b1237fabf0376b12d8e4811db
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-01-14 10:39:08 -0700

    Test: remove patient from doctor

--- spec/features/hospitals/doctors/show_spec.rb
+++ spec/features/hospitals/doctors/show_spec.rb
@@ -18,4 +18,22 @@ RSpec.describe 'Doctors Show Page' do
     expect("Patients").to appear_before("Geoff Banks")
     expect(page).to_not have_content("Wilson General")
   end
+
+  it 'displays a button next to each patient to remove that patient from the doctor' do
+    hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+    bailey = hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+    geoff = Patient.create!(name: "Geoff Banks", age: 34)
+    wilson = Patient.create!(name: "Wilson General", age: 44)
+    sharina = Patient.create!(name: "Sharina Triss", age: 22)
+    doctor_patient_1 = DoctorPatient.create!(doctor_id: bailey.id, patient_id: geoff.id)
+    doctor_patient_2 = DoctorPatient.create!(doctor_id: bailey.id, patient_id: sharina.id)
+    visit "/hospitals/#{hospital.id}/doctors/#{bailey.id}"
+    expect(page).to have_content("Geoff Banks")
+    expect(page).to have_content("Sharina Triss")
+
+    expect(page).to have_button("Remove Geoff Banks")
+    click_on("Remove Geoff Banks")
+    expect(current_path).to eq("/hospitals/#{hospital.id}/doctors/#{bailey.id}")
+    expect(page).to_not have_content("Geoff Banks")
+  end 
 end
