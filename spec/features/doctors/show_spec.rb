273b2d3fefced776c7212a61f25a60500abca0c3
Joel Grant <joelmgrant@me.com>
2022-01-14 09:43:52 -0700

    Test: Add test for seeing doctors attributes on show page

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Doctors show page' do
   before(:each) do
     @hospital1 = Hospital.create!(name: "Turing Hospital")
-    @doctor1 = @hospital.doctors.create!(name: "Doctor Turing", specialty: "ENT", university: "Turing Medical School")
+    @doctor1 = @hospital1.doctors.create!(name: "Doctor Turing", specialty: "ENT", university: "Turing Medical School")
 
     visit "/doctor/#{@doctor1.id}"
   end
 
   describe 'when I visit the doctor show page' do
     it 'shows the doctors information including name, specialty, and university' do
-      
+      expect(page).to have_content(@doctor1.name)
+      expect(page).to have_content(@doctor1.specialty)
+      expect(page).to have_content(@doctor1.university)
     end
 
     it 'shows me the name of the hospital where the doctor works' do
