fc2466724c79e6596547d9c72f06d2067aa96feb
Joel Grant <joelmgrant@me.com>
2022-01-14 10:15:23 -0700

    Test: Complete tests for User story 1/doctors show page

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe 'Doctors show page' do
     @hospital1 = Hospital.create!(name: "Turing Hospital")
     @doctor1 = @hospital1.doctors.create!(name: "Doctor Turing", specialty: "ENT", university: "Turing Medical School")
 
+    @patient1 = Patient.create!(name: "Joe Patient", age: 25)
+    @patient2 = Patient.create!(name: "Josephine Patient", age: 72)
+
+    DoctorPatient.create!(doctor_id: @doctor1.id, patient_id: @patient1.id)
+    DoctorPatient.create!(doctor_id: @doctor1.id, patient_id: @patient2.id)
+
     visit "/doctors/#{@doctor1.id}"
   end
 
@@ -16,11 +22,13 @@ RSpec.describe 'Doctors show page' do
     end
 
     it 'shows me the name of the hospital where the doctor works' do
-
+      expect(page).to have_content("Hospital: #{@hospital1.name}")
     end
 
     it 'shows the names of all of the patients this doctor has' do
-
+      expect(page).to have_content("Patients of #{@doctor1.name}")
+      expect(page).to have_content("Patient Name: #{@patient1.name}")
+      expect(page).to have_content("Patient Name: #{@patient2.name}")
     end
   end
 end
