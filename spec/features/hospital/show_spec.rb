284121e5732fdc11ed7c543b966ae0c911c6723b
Eric Chon <echon92@gmail.com>
2022-01-14 09:46:18 -0700

    created test for hospital show page

--- spec/features/hospital/show_spec.rb
+++ spec/features/hospital/show_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe 'hospital show page' do
   let!(:hospital_2) { Hospital.create!(name: "Hospital 2")}
 
   it "checks we are on the right page" do
+    visit "/hospitals/#{hospital_1.id}"
+  end
 
+  it "checks that the page has the hospitals name" do
+    visit "/hospitals/#{hospital_1.id}"
+    expect(page).to have_content(hospital_1.name)
   end
 end
