1fad515700a8e22aaeb05b71b38e715e1ac082d6
Tim Roeder <timothyjroeder@gmail.com>
2022-01-14 09:31:45 -0700

    Test: add relationship and validation tests

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -3,5 +3,11 @@ require 'rails_helper'
 RSpec.describe Hospital do
   describe 'relationships' do
     it { should have_many(:doctors) }
+    it { should have_many(:doctor_patients).through(:doctors) }
+    it { should have_many(:patients).through(:doctor_patients) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
   end
 end
