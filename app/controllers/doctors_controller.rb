ee0a5e379be1fe6bb24340af64e11488b89efd10
Sierra Tucker <sierra.tucker121@gmail.com>
2022-01-14 10:42:10 -0700

    Feat: display button to remove each patient

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,4 +2,11 @@ class DoctorsController < ApplicationController
   def show
     @doctor = Doctor.find(params[:id])
   end
+
+  def destroy
+    @doctor = Doctor.find(params[:id])
+    @patient = @doctor.patients.find(params[:patient_id])
+    @doctor.patients.delete(@patient)
+    redirect_to doctor_path(@doctor.id)
+  end
 end
