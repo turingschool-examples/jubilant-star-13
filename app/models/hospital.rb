eb951af3974dcf3503ecacf6593159246ec5f123
Brad Breiten <jonathan@breiten.us>
2022-01-14 10:30:16 -0700

    Add instance method to return all universties doctors associated with hospital have attended

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,7 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def hospital_doctor_universities
+    doctors.university_list
+  end
 end
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,21 @@ RSpec.describe Hospital do
   describe 'relationships' do
     it { should have_many(:doctors) }
   end
+
+  let!(:hospital_1) {Hospital.create!(name: "St. Mary's")}
+  let!(:hospital_2) {Hospital.create!(name: "Hospital 2")}
+
+  let!(:doctor_1) {hospital_1.doctors.create!(name: "Grey Bones", specialty: "OB/GYN", university: "Vanderbilt Medical School")}
+  let!(:doctor_2) {hospital_1.doctors.create!(name: "Dr. House", specialty: "Psychology", university: "Yale Medical")}
+  let!(:doctor_3) {hospital_1.doctors.create!(name: "Dr. Bill", specialty: "Ontology", university: "Franklin Medical")}
+  let!(:doctor_4) {hospital_2.doctors.create!(name: "Dr. Fran", specialty: "Ontology", university: "UCLA")}
+
+  describe 'instance methods' do
+    describe '#hospital_doctor_universities' do
+      it 'returns a list of universities for doctors available at that hospital' do
+        expect(hospital_1.hospital_doctor_universities).to match_array(["Vanderbilt Medical School", "Yale Medical", "Franklin Medical"])
+        expect(hospital_2.hospital_doctor_universities).to match_array(["UCLA"])
+      end
+    end
+  end
 end
