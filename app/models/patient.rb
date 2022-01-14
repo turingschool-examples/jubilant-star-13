6a521d4db6658fff786ce99a93169a8b2cd79118
Brad Breiten <jonathan@breiten.us>
2022-01-14 11:32:24 -0700

    Add instance variable to find doctor_patient record id by searching patient and doctor ids

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,10 @@
 class Patient < ApplicationRecord
   has_many :doctor_patients
   has_many :doctors, through: :doctor_patients
+
+  def find_doctor_patient_record_id(doctor_id)
+    record = doctor_patients.find_by(doctor_id: doctor_id )
+
+    record.id
+  end
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe Patient do
     it { should have_many(:doctor_patients) }
     it { should have_many(:doctors).through(:doctor_patients) }
   end
+
+  let!(:hospital_1) {Hospital.create!(name: "St. Mary's")}
+
+  let!(:doctor_1) {hospital_1.doctors.create!(name: "Grey Bones", specialty: "OB/GYN", university: "Vanderbilt Medical School")}
+  let!(:doctor_2) {hospital_1.doctors.create!(name: "Dr. House", specialty: "Psychology", university: "Yale Medical")}
+
+  let!(:patient_1) {Patient.create!(name: 'Billy Ray', age: 43)}
+
+  let!(:doctor_patient_1) {DoctorPatient.create!(doctor_id: doctor_1.id, patient_id: patient_1.id)}
+  let!(:doctor_patient_2) {DoctorPatient.create!(doctor_id: doctor_2.id, patient_id: patient_1.id)}
+
+  describe 'instance methods' do
+    describe '#find_doctor_patient_record_id' do
+      it 'returns the doctor_patient record ID with doctor_id as param' do
+        expect(patient_1.find_doctor_patient_record_id(doctor_1.id)).to eq(doctor_patient_1.id)
+        expect(patient_1.find_doctor_patient_record_id(doctor_2.id)).to eq(doctor_patient_2.id)
+      end
+    end
+  end
+
+
 end
