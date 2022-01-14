d43b995aa2b6bf8f8736c57c77d8002918e17279
Karan Mehta <karanm645@gmail.com>
2022-01-14 11:47:58 -0600

    Create show spec for hospitals

--- spec/features/hospitals/show_spec.rb
+++ spec/features/hospitals/show_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "hospital show page" do 
   let!(:three_sisters) { Hospital.create!(name: "Three Sisters Hospital") }
-  let!(:samantha) { three_sisters.doctors.create!(name: "Samanta O'Brian", specialty: "Cardiologist", university: "USC") }
+  let!(:samantha) { three_sisters.doctors.create!(name: "Samanta Mumba", specialty: "Cardiologist", university: "USC") }
+  let!(:lisa) { three_sisters.doctors.create!(name: "Lisa Left Eye Lopez", specialty: "Neurosurgeon", university: "UT") }
+  let!(:lauryn) { three_sisters.doctors.create!(name: "Lauryn Hill", specialty: "General", university: "Harvard") }
   let!(:jackie) { samantha.patients.create!(name: "Jackie Williams", age: 42)}
   let!(:lionel) { samantha.patients.create!(name: "Lionel Jackson", age: 51)}
   let!(:sarah) { samantha.patients.create!(name: "Sarah OJ", age: 32)}
@@ -18,23 +20,17 @@ RSpec.describe "hospital show page" do
     scenario 'visitor sees hospital name' do 
       expect(current_path).to eq(hospital_path(three_sisters.id))
 
-      expect(page).to have_content(samantha.name)
-      expect(page).to have_content(samantha.specialty)
-      expect(page).to have_content(samantha.university)
-      expect(page).to_not have_content(gary.name)
-    end 
-
-    scenario 'visitor sees name of hospital doctor works for' do 
       expect(page).to have_content(three_sisters.name)
       expect(page).to_not have_content(two_brothers.name)
     end 
 
-    scenario 'visitor sees name of patients doctor has' do 
-      expect(page).to have_content(jackie.name)
-      expect(page).to have_content(lionel.name)
-      expect(page).to have_content(sarah.name)
+    scenario 'visitor sees number of doctors that work in this hospital' do 
+      expect(page).to have_content(three_sisters.doctors.count)
+    end 
 
-      expect(page).to_not have_content(rod.name)
+    scenario 'visitor sees a unique list of universities that this hospitals doctors attended' do 
+
+      expect(page).to have_content("The doctors here graduated from: #{three_sisters.doctors.unique_universities}")
     end 
   end 
 end 
