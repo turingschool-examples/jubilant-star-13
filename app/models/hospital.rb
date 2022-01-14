cbf498ae8dd55983f8c4eb6a8cdb6b91adbbbd30
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-14 10:28:32 -0700

    add #doctor_count and #unique_universities

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,13 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def doctor_count
+    doctors.count
+  end
+
+  def unique_universities
+    doctors.select(:university)
+            .distinct
+            .pluck(:university)
+  end
 end
