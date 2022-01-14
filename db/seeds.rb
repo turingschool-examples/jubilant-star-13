2b2629920c40c7e75b2e81527bedbd51889e460b
Brad Breiten <jonathan@breiten.us>
2022-01-14 12:02:40 -0700

    Add seeds to check functionality on rails server

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,9 @@ Doctor.destroy_all
 
 hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
 bailey = hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+
+patient_1 = Patient.create!(name: "Billy Ray", age: 13)
+patient_2 = Patient.create!(name: "Michael Smith", age: 25)
+
+connect_1 = DoctorPatient.create!(doctor_id: bailey.id, patient_id: patient_1.id)
+connect_2 = DoctorPatient.create!(doctor_id: bailey.id, patient_id: patient_2.id)
