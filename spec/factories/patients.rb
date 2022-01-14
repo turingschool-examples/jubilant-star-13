52b5a36836fbc5b4064914c87341393c63d74cc7
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-14 09:46:17 -0700

    Create patient with doctor factory. Write doctor show page specs.

--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -2,5 +2,16 @@ FactoryBot.define do
   factory :patient do
     name { "MyString" }
     age { 1 }
+
+    factory :patient_with_doctor do
+
+      transient do
+        doctor {create(:doctor)}
+      end
+
+      after(:create) do |patient, evaluator|
+        doctor_patient = create(:doctor_patient, doctor: evaluator.doctor, patient: patient)
+      end
+    end
   end
 end
