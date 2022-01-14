17303aab2bfdc76a02bb83f022b31f1038c79f44
Tim Roeder <timothyjroeder@gmail.com>
2022-01-14 10:45:58 -0700

    Feat: add #find_by_doc_pat

--- app/models/doctor_patient.rb
+++ app/models/doctor_patient.rb
@@ -1,4 +1,9 @@
 class DoctorPatient < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
+
+  # Class Methods
+  def self.find_by_doc_pat(doctor, patient)
+    find_by(doctor: doctor, patient: patient)
+  end
 end
