10722fbc63ce3273cff91ad211251e136d358e88
Dylan Harper <dylanharper4@icloud.com>
2022-01-14 08:49:42 -0800

    Finish db setup

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,6 @@
 class Doctor < ApplicationRecord
   belongs_to :hospital
+  has_many :patient_doctors
+  has_many :patients, through: :patient_doctors
+
 end
---
+++ app/models/patient.rb
@@ -0,0 +1,5 @@
+class Patient < ApplicationRecord
+  has_many :patient_doctors
+  has_many :doctors, through: :patient_doctors
+
+end
