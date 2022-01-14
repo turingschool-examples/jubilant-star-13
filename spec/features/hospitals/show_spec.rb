2561caacb0ae35acd9a3f4cd103a1d611934f749
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-14 10:04:48 -0700

    add tests for Hosptial show page

---
+++ spec/features/hospitals/show_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'Hospital Show Page' do
+  describe 'User Story 2' do
+    before(:each) do
+      @hospital_1 = Hospital.create!(name: "Rose Medical Center")
+      @hospital_2 = Hospital.create!(name: "Swedish Medical Center")
+
+      @doctor_1 = @hospital_1.doctors.create!(name: "Dr 1", specialty: "Specialty 1", university: "University 1" )
+      @doctor_2 = @hospital_1.doctors.create!(name: "Dr 2", specialty: "Specialty 2", university: "University 1" )
+      @doctor_3 = @hospital_1.doctors.create!(name: "Dr 3", specialty: "Specialty 3", university: "University 2" )
+      @doctor_4 = @hospital_1.doctors.create!(name: "Dr 4", specialty: "Specialty 4", university: "University 2" )
+      @doctor_5 = @hospital_1.doctors.create!(name: "Dr 5", specialty: "Specialty 5", university: "University 3" )
+
+      @doctor_6 = @hospital_2.doctors.create!(name: "Dr 6", specialty: "Specialty 6", university: "University 4" )
+      @doctor_7 = @hospital_2.doctors.create!(name: "Dr 7", specialty: "Specialty 7", university: "University 5" )
+    end
+
+    xit 'has the hospital name' do
+      visit "/hospitals/#{@hospital_1.id}"
+
+      expect(page).to have_content(@hospital_1.name)
+      expect(page).to_not have_content(@hospital_2.name)
+    end
+
+    xit 'has the number of doctors that work at the hospital' do
+      visit "/hospitals/#{@hospital_1.id}"
+
+      expect(page).to have_content("Count of Doctors: 5")
+      expect(page).to_not have_content("Count of Doctors: 2")
+    end
+
+    xit 'has a unique list of universities the doctors attended' do
+      visit "/hospitals/#{@hospital_1.id}"
+
+      expect(page).to have_content("University 1", count: 1)
+      expect(page).to have_content("University 2", count: 1)
+      expect(page).to have_content("University 3", count: 1)
+      expect(page).to_not have_content("University 4")
+      expect(page).to_not have_content("University 5")
+    end
+  end
+end
