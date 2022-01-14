583954a281dfd722f5b6744bde27e7f31cf2511d
Robin Dittrich <robinodittrich@gmail.com>
2022-01-14 09:20:20 -0800

    Create action for hospitals

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,4 +1,12 @@
 class Hospital < ApplicationRecord
   has_many :doctors
   has_many :patients, through: :doctors
+
+  def doctor_count
+    doctors.count
+  end
+
+  def doctor_universities
+    doctors.pluck(:university).uniq.join(', ')
+  end
 end
