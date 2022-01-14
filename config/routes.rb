999104813931a871925b17f9ed718cb95f8c7771
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-14 11:20:52 -0700

    complete user story 3; add button to delete patient from doctor workload without completely deleting the patient; add route to delete doctor_patient and controller doctor_patient with destroy controller action

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/doctors/:id', to: 'doctors#show'
 
   get '/hospitals/:id', to: 'hospitals#show'
+
+  delete '/doctors/:id/patients/:patient_id', to: 'doctor_patients#destroy'
 end
--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -48,13 +48,50 @@ RSpec.describe 'Doctor Show Page' do
     end
 
     it 'shows the names of all the doctors patients' do
-      visit "doctors/#{@doctor_1.id}"
+      visit "/doctors/#{@doctor_1.id}"
 
       expect(page).to have_content(@patient_1.name)
       expect(page).to have_content(@patient_2.name)
       expect(page).to have_content(@patient_3.name)
       expect(page).to_not have_content(@patient_4.name)
       expect(page).to_not have_content(@patient_5.name)
+    end
+  end
+
+  describe "User Story 3" do
+    before(:each) do
+      @hospital_1 = Hospital.create!(name: "Rose Medical Center")
+      @doctor_1 = @hospital_1.doctors.create!(name: "Dr 1", specialty: "Specialty 1", university: "University 1" )
+      @doctor_2 = @hospital_1.doctors.create!(name: "Dr 2", specialty: "Specialty 2", university: "University 2" )
+      @patient_1 = Patient.create!(name: "Jake", age: 40)
+      @patient_2 = Patient.create!(name: "Alice", age: 65)
+      @patient_3 = Patient.create!(name: "Tommy", age: 10)
+
+      DoctorPatient.create!(doctor_id: @doctor_1.id, patient_id: @patient_1.id)
+      DoctorPatient.create!(doctor_id: @doctor_1.id, patient_id: @patient_2.id)
+      DoctorPatient.create!(doctor_id: @doctor_1.id, patient_id: @patient_3.id)
+
+      DoctorPatient.create!(doctor_id: @doctor_2.id, patient_id: @patient_1.id)
+      DoctorPatient.create!(doctor_id: @doctor_2.id, patient_id: @patient_2.id)
+    end
+
+    it 'has a button next to each patient name to delete from doctor assignment' do
+      visit "/doctors/#{@doctor_1.id}"
+
+      expect(page).to have_content(@patient_1.name)
+      expect(page).to have_button("Remove #{@patient_1.name}")
+      expect(page).to have_content(@patient_2.name)
+      expect(page).to have_button("Remove #{@patient_2.name}")
+      expect(page).to have_content(@patient_3.name)
+      expect(page).to have_button("Remove #{@patient_3.name}")
+
+      click_button("Remove #{@patient_1.name}")
+
+      expect(current_path).to eq("/doctors/#{@doctor_1.id}")
+
+      expect(page).to_not have_content(@patient_1.name)
+      expect(page).to have_content(@patient_2.name)
+      expect(page).to have_content(@patient_3.name)
     end
   end
 end
