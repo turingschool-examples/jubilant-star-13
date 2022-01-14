b06e0087abddae816fb17dc247284212b6dfe229
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-14 09:59:31 -0700

    Write test for doctor.hospital_name

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe Doctor do
     it { should have_many(:doctor_patients) }
     it { should have_many(:patients).through(:doctor_patients) }
   end
+
+  describe 'instance methods' do
+    describe '#hospital_name' do
+      it "returns the name of the doctors hospital" do
+        hospital = create(:hospital, name: 'Cool Hospital')
+        doctor = create(:doctor, hospital: hospital)
+
+        expect(doctor.hospital_name).to eq("Cool Hospital")
+      end
+    end
+  end
 end
