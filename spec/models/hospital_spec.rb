9eb48565c9b5729ac9c1197510aca3b96b4bb2cf
Robin Dittrich <robinodittrich@gmail.com>
2022-01-14 09:18:32 -0800

    Create model tests for patient nuiversities and count methods.

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -5,4 +5,30 @@ RSpec.describe Hospital do
     it { should have_many(:doctors) }
     it { should have_many(:patients).through(:doctors) }
   end
+  describe 'models' do
+    before(:each) do
+      @hospital_1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+
+      @doctor_1 = @hospital_1.doctors.create!(name: "Dr. X", specialty: "X-men Repair", university: "Yale")
+      @doctor_2 = @hospital_1.doctors.create!(name: "Dr. Joe", specialty: "Sleep research", university: "WWU")
+      @doctor_3 = @hospital_1.doctors.create!(name: "Dr. Sally", specialty: "Super power therapy", university: "CWU")
+      @doctor_4 = @hospital_1.doctors.create!(name: "Dr. Y", specialty: "Eye doctor", university: "Yale")
+
+      @patient_1 = Patient.create!(name: "Kayla", age: "34")
+      @patient_2 = Patient.create!(name: "Fred", age: "30")
+      @patient_3 = Patient.create!(name: "Marc", age: "50")
+
+      @patient_1.doctor_patients.create!(doctor_id: @doctor_1.id)
+      @patient_2.doctor_patients.create!(doctor_id: @doctor_1.id)
+      @patient_3.doctor_patients.create!(doctor_id: @doctor_1.id)
+    end
+
+    it '#doctor_count' do
+      expect(@hospital_1.doctor_count).to eq(4)
+    end
+
+    it '#doctor_universities' do
+      expect(@hospital_1.doctor_universities).to eq("Yale, WWU, CWU")
+    end
+  end
 end
