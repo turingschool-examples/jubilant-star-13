501203bc7d3b08ffa94ddaf03c458dff03dfa3f6
Hannah Warren <hannahkwarren@gmail.com>
2022-01-14 11:59:37 -0500

    Add hospital total doctors spec

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Hospital do
+
   describe 'relationships' do
     it { should have_many(:doctors) }
   end
+
+  it "provides a total number of doctors who work there" do 
+    hospital1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+
+    doctor1 = Doctor.create!(name: "Calliope Torres", specialty: "Orthopedics", university: "UC Berkeley Medical School", hospital_id: hospital1.id)
+    doctor2 = Doctor.create!(name: "Owen Hunt", specialty: "Trauma Surgery", university: "UC Berkeley Medical School", hospital_id: hospital1.id)
+    doctor3 = Doctor.create!(name: "Mark Sloane", specialty: "Plastic Surgery", university: "LA Med", hospital_id: hospital1.id)
+
+    expect(hospital1.total_doctors).to eq(3)
+  end
+
+
+
 end
