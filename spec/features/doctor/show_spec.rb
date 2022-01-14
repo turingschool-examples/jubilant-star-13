6643cf6d3ce58ebf2293869c2a2c5d974cf9fc82
Katy Harrod <m.catherine.harrod@gmail.com>
2022-01-14 09:48:57 -0700

    Add more testing to doctor show page and pass all specs

--- spec/features/doctor/show_spec.rb
+++ spec/features/doctor/show_spec.rb
@@ -4,12 +4,26 @@ describe 'doctor show page' do
   before do
     @hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
     @bailey = @hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+
+    @patient1 = Patient.create!(name: "Grace", age: 14)
+    @patient2 = Patient.create!(name: "Ian", age: 47)
+
+    @bailey_patient1 = PatientDoctor.create!(doctor_id: @bailey.id, patient_id: @patient1.id)
+    @bailey_patient2 = PatientDoctor.create!(doctor_id: @bailey.id, patient_id: @patient2.id)
+    visit doctor_path(@bailey)
   end
   it 'displays name, specialty, and university' do
-    visit doctor_path(@bailey)
-
     expect(page).to have_content(@bailey.name)
     expect(page).to have_content(@bailey.specialty)
     expect(page).to have_content(@bailey.university)
   end
+
+  it 'shows hospital doctor works at and all patients they care for' do
+    expect(page).to have_content(@hospital.name)
+    expect(page).to have_content(@patient1.name)
+    expect(page).to have_content(@patient2.name)
+  end
+
+  # add more patients associated w different doctor 
+  # for more robust testing after complete other stories
 end
