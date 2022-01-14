1400c6561942df9333313d8fc4fd4dd6321f3c9b
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-14 09:25:07 -0700

    Create test data

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'doctor show page' do
+  let!(:grace) {Hospital.create!(name: "Seattle Grace Memorial Hospital")}
+
+  let!(:bailey) {Doctor.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University", hospital: grace)}
+
+  let(:denny) {Patient.create!(name: "Denny Duquette", age: 39, doctor: bailey)}
+  let(:bob) {Patient.create!(name: "Bob Duquette", age: 44, doctor: bailey)}
+  let(:larry) {Patient.create!(name: "Larry Duquette", age: 27, doctor: bailey)}
   it 'displays the doctors information' do
   end
 
@@ -9,4 +16,4 @@ RSpec.describe 'doctor show page' do
 
   it 'displays the names of all the patients the doctor has' do
   end
-end 
+end
