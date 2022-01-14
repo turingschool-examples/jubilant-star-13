a806a9aeec02a4682291c4f7af855ae7501ec816
Hannah Warren <hannahkwarren@gmail.com>
2022-01-14 12:29:17 -0500

    Shore up feature tests

--- spec/features/hospitals/show_spec.rb
+++ spec/features/hospitals/show_spec.rb
@@ -13,11 +13,19 @@ RSpec.describe "Hospital show page", type: :feature do
 
     expect(page).to have_content("Grey Sloan Memorial Hospital")
     expect(page).to have_content("Doctors: 3")
-    expect(page).to have_content("UC Berkeley Medical School \n LA Med")
   end
 
-
   it "has a unique list of universities this hospital's doctors attended" do
+
+    hospital1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+
+    doctor1 = Doctor.create!(name: "Calliope Torres", specialty: "Orthopedics", university: "UC Berkeley Medical School", hospital_id: hospital1.id)
+    doctor2 = Doctor.create!(name: "Owen Hunt", specialty: "Trauma Surgery", university: "UC Berkeley Medical School", hospital_id: hospital1.id)
+    doctor3 = Doctor.create!(name: "Mark Sloane", specialty: "Plastic Surgery", university: "LA Med", hospital_id: hospital1.id)
+
+    visit hospital_path(hospital1)
+
+    expect(page).to have_content("UC Berkeley Medical School\nLA Med")
   end
 
 end
