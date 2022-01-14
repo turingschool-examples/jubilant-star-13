268d88eaad6b208b119a76abd85c8c8aab822b8f
Jessica Grazulis <jlgrazulis@gmail.com>
2022-01-14 11:05:01 -0700

    Add doctor show spec to doctors folder

---
+++ spec/features/doctors/show_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'doctor show page' do
+
+  before(:each) do
+    @grey_sloan = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+    @meredith = @grey_sloan.doctors.create!(name: "Meredith Grey", specialty: "General", university: "Dartmouth University")
+    @bailey = @grey_sloan.doctors.create!(name: "Miranda Bailey", specialty: "General", university: "Stanford University")
+    @david = @meredith.patients.create!(name: "David Hamilton", age: 67)
+    @harper = @meredith.patients.create!(name: "Harper Avery", age: 72)
+    @nick = @meredith.patients.create!(name: "Nick Marsh", age: 45)
+    @mary = @bailey.patients.create!(name: "Mary Porter", age: 35)
+  
+  end
+  scenario 'visitor sees all doctor attributes' do
+    visit "/doctors/#{@meredith.id}"
+    expect(page).to have_content(@meredith.name)
+    expect(page).to have_content(@meredith.specialty)
+    expect(page).to have_content(@meredith.university)
+  end
+
+  scenario 'visitor sees the name of the hospital where the doctor works' do
+    visit "/doctors/#{@meredith.id}"
+    expect(page).to have_content(@grey_sloan.name)
+  end
+
+  scenario 'visitor sees names of all doctors current patients' do
+    visit "/doctors/#{@meredith.id}"
+    expect(page).to have_content(@david.name)
+    expect(page).to have_content(@harper.name)
+    expect(page).to have_content(@nick.name)
+    expect(page).to_not have_content(@mary.name)
+    
+  end
+end
