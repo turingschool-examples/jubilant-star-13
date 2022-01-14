9dfbf149e08dec4d82cdc326353266c084a08606
Tim Roeder <timothyjroeder@gmail.com>
2022-01-14 09:30:39 -0700

    Feat: add relationships and validations

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,7 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+  has_many :doctor_patients, through: :doctors
+  has_many :patients, through: :doctor_patients
+
+  validates_presence_of :name 
 end
