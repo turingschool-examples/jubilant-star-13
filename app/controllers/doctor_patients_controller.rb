da72f8a8dcc5dcf0c32098ea36cfc0be0443c3ed
Tim Roeder <timothyjroeder@gmail.com>
2022-01-14 11:20:07 -0700

    Feat: create #destroy action with redirect

---
+++ app/controllers/doctor_patients_controller.rb
@@ -0,0 +1,9 @@
+class DoctorPatientsController < ApplicationController
+  def destroy
+    doctor_patient = DoctorPatient.find(params[:id])
+    doctor = Doctor.find(doctor_patient.doctor_id)
+    doctor_patient.destroy!
+    
+    redirect_to doctor_path(doctor)
+  end
+end
