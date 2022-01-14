5f23b8f5a3f1f6ff9881cd9436b6f74d1ceabae2
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-14 09:26:36 -0700

    Create a second set of doctors and patients for testing

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -4,10 +4,16 @@ RSpec.describe 'doctor show page' do
   let!(:grace) {Hospital.create!(name: "Seattle Grace Memorial Hospital")}
 
   let!(:bailey) {Doctor.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University", hospital: grace)}
+  let!(:merideth) {Doctor.create!(name: "Merideth Grey", specialty: "General Surgery", university: "Stanford University", hospital: grace)}
 
   let(:denny) {Patient.create!(name: "Denny Duquette", age: 39, doctor: bailey)}
   let(:bob) {Patient.create!(name: "Bob Duquette", age: 44, doctor: bailey)}
   let(:larry) {Patient.create!(name: "Larry Duquette", age: 27, doctor: bailey)}
+
+  let(:jeff) {Patient.create!(name: "Jeff Individual", age: 39, doctor: merideth)}
+  let(:george) {Patient.create!(name: "George Individual", age: 44, doctor: merideth)}
+  let(:hal) {Patient.create!(name: "hal Individual", age: 27, doctor: merideth)}
+
   it 'displays the doctors information' do
   end
 
